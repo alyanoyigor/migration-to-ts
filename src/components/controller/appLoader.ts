f0120d6a0dc0d38095e03832adc99c8d1558eaeb
Igor Alyanoy <igoralyanoy@gmail.com>
2022-04-14 16:33:53 +0300

    added prod files

--- src/components/controller/appLoader.ts
+++
@@ -1,9 +0,0 @@
-import { Loader } from './loader';
-
-export class AppLoader extends Loader {
-  constructor() {
-    super('https://nodenews.herokuapp.com/', {
-      apiKey: '3c36fdbbc1824a4282562c7fc76b310e',
-    });
-  }
-}
